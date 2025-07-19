e829ff971eb442e6b8c538b0b72636e8bd8e4e9e
Fuadseid <fuaddbus@gmail.com>
2025-07-19 22:00:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React + Webpack 🚀</h1>
+      <p>This app was set up manually!</p>
+    </div>
+  );
+}
+
+export default App;
